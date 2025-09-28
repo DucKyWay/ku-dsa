d75c8e3ec1c8eff5489923fc4d58bc041e274231
DucKyWay <niji2549@gmail.com>
2025-09-28 09:26:50 +0700

    catch up

--- w09/magicvein.cpp
+++ w09/magicvein.cpp
@@ -3,133 +3,133 @@
 using namespace std;
 
 struct Node {
-    long long val;
-    Node* next;
-    Node(long long v): val(v), next(nullptr) {}
+    int val;
+    Node* next = nullptr;
+    Node(int v): val(v), next(nullptr) {}
 };
 
 class LinkedList {
-private:
-    Node* head;
-    Node* tail;
-    size_t length;
+    private:
+        Node* head;
+        Node* tail;
+        size_t length;
 
-    // mergesort
-    Node* merge(Node* left, Node* right) {
-        if (!left) 
-            return right;
-        if (!right) 
-            return left;
+        // mergesort
+        Node* merge(Node* left, Node* right) {
+            if (!left) 
+                return right;
+            if (!right) 
+                return left;
 
-        Node dummy(0);
-        Node* current = &dummy;
+            Node dummy(0);
+            Node* curr = &dummy;
 
-        while (left && right) {
-            if (left->val <= right->val) {
-                current->next = left;
-                left = left->next;
-            } else {
-                current->next = right;
-                right = right->next;
+            while (left && right) {
+                if (left->val <= right->val) {
+                    curr->next = left;
+                    left = left->next;
+                } else {
+                    curr->next = right;
+                    right = right->next;
+                }
+                curr = curr->next;
             }
-            current = current->next;
+            
+            curr->next = (left ? left : right);
+            return dummy.next;
+        }
+        
+        Node* split(Node* start) {
+            if (!start || !start->next) return nullptr;
+
+            Node* slow = start;
+            Node* fast = start->next;
+
+            while (fast && fast->next) {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            
+            Node* mid = slow->next;
+            slow->next = nullptr;
+            return mid;
         }
 
-        current->next = (left ? left : right);
-        return dummy.next;
-    }
+        Node* mergeSort(Node* node) {
+            if (!node || !node->next) 
+                return node;
 
-    Node* split(Node* start) {
-        if (!start || !start->next) return nullptr;
+            Node* mid = split(node);
+            Node* left = mergeSort(node);
+            Node* right = mergeSort(mid);
 
-        Node* slow = start;
-        Node* fast = start->next;
-
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+            return merge(left, right);
+        }
+        
+    public: 
+        LinkedList() : head(nullptr), tail(nullptr), length(0) {}
+        ~LinkedList() { clear(); }
+        
+        void push_back(int v) {
+            Node* newNode = new Node(v);
+            if (!head) {
+                head = tail = newNode;
+            } else {
+                tail->next = newNode;
+                tail = newNode;
+            }
+            length++;
         }
 
-        Node* mid = slow->next;
-        slow->next = nullptr;
-        return mid;
-    }
+        void sort() {
+            head = mergeSort(head);
 
-    Node* mergeSort(Node* node) {
-        if (!node || !node->next) 
-            return node;
-
-        Node* mid = split(node);
-        Node* left = mergeSort(node);
-        Node* right = mergeSort(mid);
-
-        return merge(left, right);
-    }
-
-public:
-    LinkedList() : head(nullptr), tail(nullptr), length(0) {}
-    ~LinkedList() { clear(); }
-
-    void push_back(long long v) {
-        Node* newNode = new Node(v);
-        if (!head) {
-            head = tail = newNode;
-        } else {
-            tail->next = newNode;
-            tail = newNode;
-        }
-        length++;
-    }
-
-    void sort() {
-        head = mergeSort(head);
-
-        tail = head;
-        while (tail && tail->next) {
-            tail = tail->next;
-        }
-    }
-
-    bool empty() const {
-        return head == nullptr;
-    }
-
-    void clear() {
-        Node* current = head;
-        while (current) {
-            Node* nextNode = current->next;
-            delete current;
-            current = nextNode;
-        }
-        head = tail = nullptr;
-        length = 0;
-    }
-
-    Node* getHead() const { return head; }
-
-    void print() const {
-        if (empty()) {
-            cout << "IMPOSSIBLE\n";
-            return;
+            tail = head;
+            while (tail && tail->next) {
+                tail = tail->next;
+            }
         }
 
-        Node* current = head;
-        bool first = true;
-        while (current) {
-            if (!first) cout << " ";
-            cout << current->val;
-            first = false;
-            current = current->next;
+        bool empty() {
+            return head == nullptr;
         }
-        cout << "\n";
-    }
+
+        void clear() {
+            Node* curr = head;
+            while (curr) {
+                Node* nextNode = curr->next;
+                delete curr;
+                curr = nextNode;
+            }
+            head = tail = nullptr;
+            length = 0;
+        }
+
+        Node* getHead() { return head; }
+
+        void print() {
+            if (empty()) {
+                cout << "IMPOSSIBLE" << endl;
+                return;
+            }
+
+            Node* curr = head;
+            bool first = true;
+            while (curr) {
+                if (!first) cout << " ";
+                cout << curr->val;
+                first = false;
+                curr = curr->next;
+            }
+            cout << endl;
+        }
 };
 
-bool isPrime(long long n) {
+bool isPrime(int n) {
     if (n <= 1)     return false;
     if (n % 2 == 0) return n == 2;
     if (n % 3 == 0) return n == 3;
-    for (long long i = 5; i * i <= n; i += 6) {
+    for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) 
             return false;
     }
@@ -143,44 +143,40 @@ int main() {
     int x;
     cin >> x;
 
-    while (x--) {
+    for (int i = 0; i < x; i++) {
         int m;
         cin >> m;
 
-        LinkedList allGroups;
-        LinkedList result;
+        LinkedList all, res;
 
-        for (int i = 0; i < m; i++) {
-            long long amount;
+        for (int j = 0; j < m; j++) {
+            int amount;
             cin >> amount;
-            if (amount >= 0) {
-                allGroups.push_back(amount);
+            if (amount > 0) {
+                all.push_back(amount);
             }
         }
 
-        long long minLimit, maxLimit;
-        cin >> minLimit >> maxLimit;
-        if (minLimit > maxLimit) swap(minLimit, maxLimit);
+        int minLim, maxLim;
+        cin >> minLim >> maxLim;
+        if (minLim > maxLim) swap(minLim, maxLim);
 
         // คุ้มมมมมมมมมมมมมมม
-        for (Node* cur = allGroups.getHead(); cur != nullptr; cur = cur->next) {
-            long long v = cur->val;
+        for (Node* cur = all.getHead(); cur != nullptr; cur = cur->next) {
+            int v = cur->val;
 
             bool valid = false;
-            if (v >= minLimit && v <= maxLimit) valid = true;
+            if (v >= minLim && v <= maxLim) valid = true;
             if (isPrime(v)) valid = true;
             if (v % 7 == 0) valid = true;
 
             if (valid) {
-                result.push_back(v);
+                res.push_back(v);
             }
         }
 
-        result.sort();
-        result.print();
-
-        allGroups.clear();
-        result.clear();
+        res.sort();
+        res.print();
     }
 
     return 0;
