6663fd64b4785f7fbf009133b1b260576902aa9f
DucKyWay <niji2549@gmail.com>
2025-10-19 08:25:00 +0700

    catch up

---
+++ w10/orbs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+
+    int n;
+    cin >> n;
+    vector<int> power(n);
+    for(int i = 0; i < n; i++) {
+        cin >> power[i];
+    }
+
+
+
+    return 0;
+}
